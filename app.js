cdc97b4879ba2fc2f1c33ceb48da3a69f7e59cd6
Bhatt <d_bhatt8@tss-hr420-dm25.local>
2019-03-21 17:34:44 -0400

    add files

---
+++ app.js
@@ -0,0 +1,20 @@
+var express = require('express');
+var app = express();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+const port = process.env.PORT || 3000;
+
+
+
+
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+
+    res.sendFile(__dirname + '/views/index.html');
+});
+
+http.listen(port, () => {
+    console.log(`app is running on port ${port}`);
+});
